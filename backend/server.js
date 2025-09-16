bc0d40e5f3f141c4ed62c3da1e8d8a47dc914958
gowrichandana24 <gowrichandanap2005@gmail.com>
2025-09-16 12:47:39 +0530

    push fi

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '------',  
+    password: '-----',  
     database: 'Students'   
 });
 
